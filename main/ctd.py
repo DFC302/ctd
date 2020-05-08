bf8d155f31cbeb2ee9e46f40d32fa5b243d1ddf0
Matthew Greer <mgreer906@gmail.com>
2020-05-08 00:31:20 -0400

    Add files via upload

--- main/ctd.py
+++ main/ctd.py
@@ -1,1 +1,53 @@
-a
+#!/usr/bin/env python3
+
+import sys
+import difflib
+from colorama import Fore, Style
+
+def compare():
+	green = Fore.GREEN
+	red = Fore.RED
+	yellow = Fore.YELLOW 
+	reset = Fore.RESET
+	 
+	original = sys.argv[1]
+	compare = sys.argv[2]
+
+	combine_org = []
+	combine_comp = []
+
+	for letter in difflib.ndiff(original, compare):
+		if letter[0] == "-":
+			combine_org.append(f"{red}{letter[2]}")
+
+		elif letter[0] == " ":
+			combine_org.append(f"{green}{letter}")
+
+	for letter in difflib.ndiff(original, compare):
+		if letter[0] == "+":
+			combine_comp.append(f"{red}{letter[2]}")
+
+		elif letter[0] == " ":
+			combine_comp.append(f"{green}{letter}")
+
+	show_diff_org = "".join(combine_org).replace(" ","")
+	show_diff_comp = "".join(combine_comp).replace(" ","")
+
+	print(f"First:\t{yellow}{original} | {reset}Difference: {red}{show_diff_org}{reset}")
+	print(f"Second:\t{yellow}{compare} | {reset}Difference: {red}{show_diff_comp}{reset}")
+
+
+def main():
+	if len(sys.argv) < 3:
+		print("Missing comparison string!")
+		sys.exit(1)
+
+	elif len(sys.argv) > 3:
+		print("Too many strings to compare!")
+		sys.exit(1)
+		
+	elif len(sys.argv) == 3:
+		compare()
+
+if __name__ == "__main__":
+	main()
